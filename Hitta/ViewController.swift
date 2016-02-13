beae6caf07874c7e4355d4b34283fa1bf2c7a37f
GreatApe <kugel@kth.se>
2016-02-14 00:04:18 +0100

    Final?

--- Hitta/ViewController.swift
+++ Hitta/ViewController.swift
@@ -12,8 +12,8 @@ import MapKit
 let tranholmen = CLLocationCoordinate2D(latitude: 59.375129, longitude: 18.087906)
 
 class ViewController: UITableViewController {
-    var region = MKCoordinateRegion(center: tranholmen, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-    var location = tranholmen
+    var region: MKCoordinateRegion! = MKCoordinateRegion(center: tranholmen, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    var location: CLLocationCoordinate2D! = tranholmen
     
     @IBOutlet weak var dismissMapButton: UIButton!
     @IBOutlet weak var mapView: MKMapView!
